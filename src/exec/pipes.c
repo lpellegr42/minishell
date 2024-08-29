cfcac2b6bfc95c5b0da097d034e5246bdf3a4ddd
livsauze <sauzetmartinlivane@hotmail.com>
2024-08-29 19:32:39 +0200

    cat | cat | ls ok ?

--- src/exec/pipes.c
+++ src/exec/pipes.c
@@ -17,25 +17,48 @@ pid_t	create_child_process(void)
 	return (pid);
 }
 
-void	child_process_exec(t_all *all, int input_fd, int *pipe_fd)
+void	handle_child_process(t_all *all, int input_fd, int pipe_fd[2])
 {
 	if (all->data->next != NULL)
-		dup2(pipe_fd[1], STDOUT_FILENO);
-	close(pipe_fd[1]);
-	dup2(input_fd, STDIN_FILENO);
+	{
+		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+			perror_and_exit("dup2 error");
+	}
 	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	if (dup2(input_fd, STDIN_FILENO) == -1)
+		perror_and_exit("dup2 error");
+	if (input_fd != 0)
+		close(input_fd);
 	ft_choose_cmd(all);
+	if (access(all->data->cmd, F_OK | X_OK) == -1)
+		all->err = 127; // Commande introuvable, on sort avec le code 127
 	exit(all->err);
 }
 
-void	parent_process(t_all *all, int pipe_fd[2], int *input_fd, pid_t pid)
+void	handle_parent_process(int *input_fd, int pipe_fd[2])
+{
+	close(pipe_fd[1]);
+	if (*input_fd != 0)
+		close(*input_fd);
+	*input_fd = pipe_fd[0];
+}
+
+void	wait_for_children(t_all *all)
 {
 	int	status;
 
-	waitpid(pid, &status, 0);
-	all->err = (status >> 8);
-	close(pipe_fd[1]);
-	*input_fd = pipe_fd[0];
+	while (waitpid(-1, &status, 0) > 0)
+	{
+		if (WIFEXITED(status))
+			all->err = WEXITSTATUS(status);
+	}
+}
+
+void	initialize_pipe(int pipe_fd[2])
+{
+	if (pipe(pipe_fd) == -1)
+		perror_and_exit("Pipe error");
 }
 
 void	ft_do_pipe(t_all *all)
@@ -43,25 +66,25 @@ void	ft_do_pipe(t_all *all)
 	int		pipe_fd[2];
 	pid_t	pid;
 	int		input_fd;
+	// int		status;
 
 	input_fd = 0;
 	while (all->data)
 	{
-		if (pipe(pipe_fd) == -1)
-			perror_and_exit("Pipe error");
+		initialize_pipe(pipe_fd);
 		pid = create_child_process();
 		if (pid == 0)
-			child_process_exec(all, input_fd, pipe_fd);
-			//free_funct_there ?
+			handle_child_process(all, input_fd, pipe_fd);
 		else
 		{
-			parent_process(all, pipe_fd, &input_fd, pid);
+			handle_parent_process(&input_fd, pipe_fd);
 			if (!all->data->next)
 			{
 				close(pipe_fd[0]);
-				return ;
+				break;
 			}
 			all->data = all->data->next;
 		}
 	}
+	wait_for_children(all);
 }
