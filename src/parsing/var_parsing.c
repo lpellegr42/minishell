e552745078e5ad48c184bfcc7bde477f5aa6f7c8
lpellegr42 <leoplg42@gmail.com>
2024-09-03 12:47:03 +0200

    added starting var funct

--- src/parsing/var_parsing.c
+++ src/parsing/var_parsing.c
@@ -1,2 +1,46 @@
 #include "../../includes/minishell.h"
 
+char	*get_var(char *line, int i, t_all *all)
+{
+	char	*str;
+	char	*var;
+	int		flag;
+	int		j;
+	int		k;
+
+	k = 0;
+	flag = is_digit(line[i]);
+	// if (check_var(line))
+	if (line[i] == '?')
+	{
+		str = ft_itoa(all->err);
+		all->err = 0;
+		return (str);
+	}
+	if (flag)
+		i++;
+	j = i;
+	while (line[j] && line[j] != ' ')
+		j++;
+	str = malloc(sizeof(char) * (j - i + 1));
+	while (i < j)
+	{
+		str[k] = line[i];
+		i++;
+		k++;
+	}
+	str[k] = '\0';
+	if (!flag)
+	{
+		var = ft_getenv(str, all->env);
+		return (free(str), var);
+	}
+	else
+		return (str);
+}
+
+/*
+	if (res[i][0] == '$')
+		data->arg[j] = my_strdup(get_var(res[i], 1, all));
+*/
+
