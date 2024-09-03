42e25578903df951e00019a4a0eb7d111201f5d9
lpellegr42 <leoplg42@gmail.com>
2024-09-03 13:49:59 +0200

    starting var env

--- src/parsing/init_parsing.c
+++ src/parsing/init_parsing.c
@@ -36,7 +36,7 @@ t_all	*parsing(char *line, t_all *all)
 	while (node)
 	{
 		node = fill_args(node);
-		//clean et remplissage des redir ici, puis clean des dernieres quotes.
+		node = replace_var(node); //clean et remplissage des redir ici, puis clean des dernieres quotes.
 		apply_clean_on_args(node);
 		node = node->next;
 	}
